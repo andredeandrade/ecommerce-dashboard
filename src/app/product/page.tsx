3281957574ac8a00ec827fdc382e7871323c954d
André <andre@andrade.inf.br>
2025-09-30 21:31:01 -0300

    feat: add chakra ui

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,13 @@
+import { Button, HStack } from '@chakra-ui/react'
+
 export default function Product() {
   return (
     <div>
       <h1>Product Page</h1>
+      <HStack gap="4" mt="10" ml="4">
+        <Button variant="outline">Click me</Button>
+        <Button colorPalette="green">Click me</Button>
+      </HStack>
     </div>
   )
 }
